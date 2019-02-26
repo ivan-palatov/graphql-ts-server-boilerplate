f0d65afaa8d3b9bb759a7fbf15f3347629f4e46e
Ivan <ivanakazver3d@gmail.com>
2019-02-26 17:50:48 +0700

    Resolve some issues, add register basic functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import 'reflect-metadata';
 import { GraphQLServer } from 'graphql-yoga';
 import { importSchema } from 'graphql-import';
+import * as path from 'path';
 
 import { resolvers } from './resolvers';
+import { createConnection } from 'typeorm';
 
-const typeDefs = importSchema('./schema.graphql');
+const typeDefs = importSchema(path.join(__dirname, 'schema.graphql'));
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => {
+(async () => {
+  await createConnection();
+  console.log('Connected to DB.');
+  const server = new GraphQLServer({ typeDefs, resolvers });
+  await server.start();
   console.clear();
   console.log('Server is running at http://localhost:4000');
-});
+})();
