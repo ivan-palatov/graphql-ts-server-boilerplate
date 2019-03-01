d72c9fe438af79793cbaed03eea071bffcf2cf05
Ivan <ivanakazver3d@gmail.com>
2019-03-01 18:56:49 +0700

    Change logout to logout from all devices and browsers at once

--- src/modules/logout/resolvers.ts
+++ src/modules/logout/resolvers.ts
@@ -9,9 +9,11 @@ export const resolvers: IResolverMap = {
         return true;
       }
       const sessionIds = await redis.lrange(`${USER_SESSION_ID_PREFIX}${userId}`, 0, -1);
+      const promises: Array<Promise<number>> = [];
       for (const sessionId of sessionIds) {
-        await redis.del(`${REDIS_SESSION_PREFIX}${sessionId}`);
+        promises.push(redis.del(`${REDIS_SESSION_PREFIX}${sessionId}`));
       }
+      await Promise.all(promises);
       return true;
     },
   },
